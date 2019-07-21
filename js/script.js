f1d9d991db26ac98dcca5c6eed68fcf95d39bccd
Annanaza <38279241+Annanaza@users.noreply.github.com>
2019-07-21 16:35:51 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let elem = document.querySelector("#elem");
+let body = document.querySelector("body");
+
+elem.onclick = function() {
+body.classList.toggle("invert");
+}
+
+// //
+// let loading = [".", "..", "...", "...."];
+// let i = -1;
+// setInterval(function() {
+// if (i < loading.length - 1) {
+// i++;
+// }
+// else {
+// i = 0;
+// }
+// console.log(loading[i]);
+// }, 500)
+
+let loading = [".", "..", "...", "...."];
+let i = 0;
+setInterval(function() {
+console.log(loading[i++ % loading.length]);
+}, 500);
